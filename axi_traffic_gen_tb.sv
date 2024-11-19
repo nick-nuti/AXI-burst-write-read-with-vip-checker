dec1c0a0d0914c3c527febcf59910437849d2750
Nicholas Nuti <54613418+nick-nuti@users.noreply.github.com>
2024-11-18 21:24:00 -0800

    Update axi_traffic_gen_tb.sv

--- axi_traffic_gen_tb.sv
+++ axi_traffic_gen_tb.sv
@@ -1,273 +1,328 @@
-
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date: 07/02/2024 12:15:09 AM
-// Design Name:
-// Module Name: nnuti_axi3_traffic_generator
-// Project Name:
-// Target Devices:
-// Tool Versions:
-// Description:
-//
-// Dependencies:
-//
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/02/2024 11:48:24 PM
+// Design Name: 
+// Module Name: axi_traffic_gen_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-//
+// 
 //////////////////////////////////////////////////////////////////////////////////
 
-/*
+import axi_vip_pkg::*;
+import design_1_axi_vip_0_0_pkg::*;
 
-// Things I would like to add:
-- parameters for WRITE and READ to generate logic...
-- method to exit to IDLE if slave doesn't finish the write or read... (leaves us idle for X or more clock cycles)
-- parameter to change IP to handle AXI3 or AXI4 parameters
+module axi_traffic_gen_tb();
+//
+xil_axi_uint slv_mem_agent_verbosity = 0;
+design_1_axi_vip_0_0_slv_mem_t slv_mem_agent;
 
-AXI3
-- no QoS
-- 16 beats in a burst max
-AXI4
-- adds "fixed-length bursts"
-- yes QoS
-- supports atomic (ex: read-modify-write transactions...) complete without interference
-- supports AXI4-lite and AXI4-stream
-- 256 beats in a burst max
-*/
+//
+reg aclk;
+reg aresetn;
+wire aresetn_out;
+//
+// singlex2, 16, singlex3, 16, 16, singlex2
 
-module axi_traffic_gen #(
-    parameter ADDR_W=32,
-    parameter DATA_W=64
-)
-(
-  /**************** Write Address Channel Signals ****************/
-  output reg [ADDR_W-1:0]              m_axi_awaddr, // address (done)
-  output reg [3-1:0]                   m_axi_awprot = 3'b000, // protection - privilege and securit level of transaction
-  output reg                           m_axi_awvalid, // (done)
-  input  wire                          m_axi_awready, // (done)
-  output reg [3-1:0]                   m_axi_awsize = $clog2(DATA_W/8), //3'b011, // burst size - size of each transfer in the burst 3'b011 for 8 bytes
-  output reg [2-1:0]                   m_axi_awburst = 2'b01, // fixed burst = 00, incremental = 01, wrapped burst = 10
-  output reg [4-1:0]                   m_axi_awcache = 4'b0000, //4'b0011, // cache type - how transaction interacts with caches
-  output reg [4-1:0]                   m_axi_awlen, // number of data transfers in the burst (0-255) (done)
-  output reg [1-1:0]                   m_axi_awlock = 1'b0, // lock type - indicates if transaction is part of locked sequence
-  output reg [4-1:0]                   m_axi_awqos = 4'b0000, // quality of service - transaction indication of priority level
-  output reg [4-1:0]                   m_axi_awregion = 4'b0000, // region identifier - identifies targetted region
-  /**************** Write Data Channel Signals ****************/
-  output reg [DATA_W-1:0]              m_axi_wdata, // (done)
-  output reg [DATA_W/8-1:0]            m_axi_wstrb, // (done)
-  output reg                           m_axi_wvalid, // set to 1 when data is ready to be transferred (done)
-  input  wire                          m_axi_wready, // (done)
-  output reg                           m_axi_wlast, // if awlen=0 then set wlast (done)
-  /**************** Write Response Channel Signals ****************/
-  input  wire [2-1:0]                  m_axi_bresp, // (done) write response - status of the write transaction (00 = okay, 01 = exokay, 10 = slverr, 11 = decerr)
-  input  wire                          m_axi_bvalid, // (done) write response valid - 0 = response not valid, 1 = response is valid
-  output reg                           m_axi_bready, // (done) write response ready - 0 = not ready, 1 = ready
-  /**************** Read Address Channel Signals ****************/
-  output reg [ADDR_W-1:0]              m_axi_araddr, // address
-  output reg [3-1:0]                   m_axi_arprot = 3'b000, // protection - privilege and securit level of transaction
-  output reg                           m_axi_arvalid, // 
-  input  wire                          m_axi_arready, // 
-  output reg [3-1:0]                   m_axi_arsize = $clog2(DATA_W/8), //3'b011, // burst size - size of each transfer in the burst 3'b011 for 8 bytes
-  output reg [2-1:0]                   m_axi_arburst = 2'b01, // fixed burst = 00, incremental = 01, wrapped burst = 10
-  output reg [4-1:0]                   m_axi_arcache = 4'b0000, //4'b0011, // cache type - how transaction interacts with caches
-  output reg [4-1:0]                   m_axi_arlen, // number of data transfers in the burst (0-255) (done)
-  output reg [1-1:0]                   m_axi_arlock = 1'b0, // lock type - indicates if transaction is part of locked sequence
-  output reg [4-1:0]                   m_axi_arqos = 4'b0000, // quality of service - transaction indication of priority level
-  output reg [4-1:0]                   m_axi_arregion = 4'b0000, // region identifier - identifies targetted region
-  /**************** Read Data Channel Signals ****************/
-  output reg                           m_axi_rready, // read ready - 0 = not ready, 1 = ready
-  input  wire [DATA_W-1:0]             m_axi_rdata, // 
-  input  wire                          m_axi_rvalid, // read response valid - 0 = response not valid, 1 = response is valid
-  input  wire                          m_axi_rlast, // =1 when on last read
-  /**************** Read Response Channel Signals ****************/
-  input  wire [2-1:0]                  m_axi_rresp, // read response - status of the read transaction (00 = okay, 01 = exokay, 10 = slverr, 11 = decerr)
-  /**************** System Signals ****************/
-  input wire                           aclk,
-  input wire                           aresetn,
-  /**************** User Control Signals ****************/  
-  input  wire                          user_start,
-  input  wire                          user_w_r, // 0 = write, 1 = read
-  input  wire [3:0]                    user_burst_len_in,
-  input  wire [DATA_W/8-1:0]           user_data_strb,
-  input  wire [DATA_W-1:0]             user_data_in,
-  input  wire [ADDR_W-1:0]             user_addr_in,
-  output reg                           user_free,
-  output reg                           user_stall_data, // can this be caused by all of these: m_axi_awready, m_axi_awvalid, m_axi_wvalid, m_axi_wready
-  output reg  [1:0]                    user_status,
-  output reg  [DATA_W-1:0]             user_data_out,
-  output reg                           user_data_out_en
-);
-   
-// AXI FSM ---------------------------------------------------
-    localparam IDLE             = 3'b000;
-    localparam WRITE            = 3'b001;
-    localparam WRITE_RESPONSE   = 3'b010;
-    localparam READ_RESPONSE    = 3'b011;
-    localparam DEACTIVATE_START = 3'b100;
-       
-    reg [2:0] axi_cs, axi_ns;
-    reg [7:0] w_data_counter;
-   
-    always @ (posedge aclk or negedge aresetn)
+`define ADDR_W 32
+`define DATA_W 64
+
+reg  [`ADDR_W-1:0] u_addr [0:9] = 
+{
+    'h10000000, //0
+    'h10000040, //0
+    'h10000080, //15
+    'h10000C00, //0
+    'h10000C40, //0
+    'h10000C80, //0
+    'h10000CC0, //15
+    'h100010C0, //15
+    'h10001500, //0
+    'h10001540  //0
+};
+
+reg  [3:0]  u_b_len [0:9] =
+{
+'h0,
+'h0,
+'d15,
+'h0,
+'h0,
+'h0,
+'d15,
+'d15,
+'h0,
+'h0
+};
+
+bit  [`DATA_W-1:0] u_data_in [0:54] =
+{
+'hF8F4F2F1,
+'h87654321,
+'h0000000A,'h000000BA,'h00000CBA,'h0000DCBA,'h000EDCBA,'h00FEDCBA,'h0AFEDCBA,'hBAFEDCBA,'h0BAFEDCB,'h00BAFEDC,'h000BAFED,'h0000BAFE,'h00000BAF,'h000000BA,'h0000000B,'h00000000,
+'h12345678,
+'h08060402,
+'h07050301,
+'h1000000A,'h200000BA,'h30000CBA,'h4000DCBA,'h500EDCBA,'h60FEDCBA,'h7AFEDCBA,'h8AFEDCBA,'h9BAFEDCB,'hA0BAFEDC,'hB00BAFED,'hC000BAFE,'hD0000BAF,'hE00000BA,'hF000000B,'h10000000,
+'h00000000,'h11111111,'h22222222,'h33333333,'h44444444,'h55555555,'h66666666,'h77777777,'h88888888,'h99999999,'hAAAAAAAA,'hBBBBBBBB,'hCCCCCCCC,'hDDDDDDDD,'hEEEEEEEE,'hFFFFFFFF,
+'hBADCAFEE,
+'hDEADBEEF
+};
+
+bit  [`DATA_W-1:0] u_data_out [0:54];
+
+bit  [`DATA_W-1:0] cmp_data_diff;
+
+reg   [7:0] u_pix_len [0:9] =
+{
+'b11111111,
+'b11111111,
+'b11111111,
+'b11111111,
+'b11111111,
+'b11111111,
+'b00001111,
+'b11110000,
+'b00000001,
+'b11100000
+};
+
+reg         user_start;
+
+wire        user_free;
+wire        user_stall_data;
+wire [1:0]  user_status;
+//
+
+reg  [`ADDR_W-1:0] user_addr_in;
+reg  [3:0]  user_burst_len_in;
+bit  [`DATA_W-1:0] user_data_in;
+bit  [`DATA_W-1:0] user_data_out;
+reg         user_data_out_en;
+reg  [7:0]  user_pixels_1_2;
+int         running_index;
+//
+reg axi_ready;
+//
+reg user_w_r;
+//
+reg compare_w_r_arrays;
+int cmp_it;
+longint current_addr;
+
+integer file, i;
+
+initial
+begin
+    axi_ready = 0;
+    slv_mem_agent = new("slave vip agent",d1w0.design_1_i.axi_vip_0.inst.IF);
+    slv_mem_agent.set_agent_tag("Slave VIP");
+    slv_mem_agent.set_verbosity(slv_mem_agent_verbosity);
+    slv_mem_agent.start_slave();
+    //slv_mem_agent.mem_model.pre_load_mem("compile.sh", 0);
+    slv_mem_agent.mem_model.pre_load_mem("vip_mem_out.mem", 0);
+    //slv_mem_agent.mem_model.set_mem_depth(1024);
+
+    axi_ready = 1;
+end
+
+initial
+begin
+    aclk = 0;
+    aresetn = 0;
+    user_addr_in = 'h0;
+    user_burst_len_in = 'h0;
+    user_data_in = 'h0;
+    user_pixels_1_2 = 'h0;
+    user_start = 'h0;
+    user_w_r = 'h0;
+    compare_w_r_arrays = 0;
+end
+
+always
+begin
+    #8ns aclk = ~aclk;
+end
+
+initial
+begin
+    wait(axi_ready);
+    aresetn = 1;
+    #5us;
+    
+    #10us;
+
+//AXI WRITES    
+    @(posedge aclk);
+    user_start      = 1'd0;
+    running_index   = 'd0;
+
+    for(int i = 0; i < 10; i++)
     begin
-        if(~aresetn)
-        begin
-            axi_cs <= IDLE;
-        end
-       
-        else
-        begin
-            axi_cs <= axi_ns;
-        end
-    end
-   
-    always @ (*)
-    begin
-        case(axi_cs)
-        IDLE:
-        begin
-            if(m_axi_awready & user_start & ~user_w_r)
-            begin
-                axi_ns = WRITE;
-            end
-
-            else if(m_axi_arready & user_start & user_w_r)
-            begin
-                axi_ns = READ_RESPONSE;
-            end
-           
-            else
-            begin
-                axi_ns = IDLE;
-            end
-        end
-       
-        WRITE:
-        begin
-            if((w_data_counter == user_burst_len_in) && m_axi_wready)
-            begin
-                axi_ns = WRITE_RESPONSE;
-            end
-           
-            else
-            begin
-                axi_ns = WRITE;
-            end
-        end
-       
-        WRITE_RESPONSE:
-        begin
-            if(m_axi_bvalid)
-            begin
-                if(user_start) axi_ns = DEACTIVATE_START;
-                else axi_ns = IDLE;
-            end
-            else axi_ns = WRITE_RESPONSE;
-        end
-
-        READ_RESPONSE:
-        begin
-            if(m_axi_rlast & m_axi_rvalid)
-            begin
-                if(user_start) axi_ns = DEACTIVATE_START;
-                else axi_ns = IDLE;
-            end
-           
-            else
-            begin
-                axi_ns = READ_RESPONSE;
-            end
-        end
+        wait(user_free);
+        @(posedge aclk);
+    
+        user_addr_in        = u_addr[i];
+        user_burst_len_in   = u_b_len[i];
+        user_pixels_1_2     = u_pix_len[0]; //u_pix_len[i];
+        user_data_in        = u_data_in[running_index];
+        @(posedge aclk);
+        user_start          = 1'd1;
+        running_index++;
         
-        DEACTIVATE_START:
+        @(posedge aclk);
+        wait(~user_free);
+        
+        if(u_b_len[i] > 'd0)
         begin
-            if(user_start) axi_ns = DEACTIVATE_START;
-            else axi_ns = IDLE;
+            for(int b = 0; b < u_b_len[i]; b++)
+            begin
+           
+                
+                //running_index++;
+                @(negedge user_stall_data);
+                @(posedge aclk);
+                
+                //@(posedge aclk_out);
+                user_data_in = u_data_in[running_index];
+                running_index++;
+            end
         end
-       
-        default: axi_ns = IDLE;
-        endcase
-    end
-
-// AXI WRITE ---------------------------------------------------
-    always @ (posedge aclk)
-    begin
-//
-        if(axi_cs == IDLE || axi_cs == WRITE_RESPONSE) w_data_counter <= 'h0;
-       
-        else if(axi_cs == WRITE && m_axi_wready && w_data_counter < user_burst_len_in)
-        begin
-            w_data_counter <= w_data_counter + 1'b1;
-        end
-       
-        else w_data_counter <= w_data_counter;
-//
+        user_start          = 1'd0;
     end
     
-    always @ (*)
+//AXI READS
+    @(posedge aclk);
+    user_start      = 1'd0;
+    running_index   = 'd0;
+    user_w_r = 'h1;
+
+    for(int i = 0; i < 10; i++)
     begin
-        m_axi_awvalid <= ((axi_cs==IDLE) && (axi_ns==WRITE)) ? 1 : 0;
-        m_axi_awlen   <= ((axi_cs==IDLE) && (axi_ns==WRITE)) ? user_burst_len_in : 0;
-        m_axi_awaddr  <= ((axi_cs==IDLE) && (axi_ns==WRITE)) ? user_addr_in : 0;
-        m_axi_wvalid  <= (axi_cs==WRITE) ? 1 : 0;
-        m_axi_wdata   <= (axi_cs==WRITE) ? user_data_in : 0;
-        m_axi_wstrb   <= (axi_cs==WRITE) ? user_data_strb : 0;
-        m_axi_wlast   <= ((axi_cs==WRITE)&&(w_data_counter == user_burst_len_in)) ? 1'b1 : 1'b0;
-        m_axi_bready  <= ((axi_cs == WRITE_RESPONSE)&& m_axi_bvalid) ? 1'b1 : 'h0;
+        wait(user_free);
+        @(posedge aclk);
+    
+        user_addr_in        = u_addr[i];
+        user_burst_len_in   = u_b_len[i];
+        user_start          = 1'd1;
+     
+        @(posedge user_stall_data);
+        //wait(user_data_out_en == 1);
+        //user_start          = 1'd0;
+        //@(posedge aclk);
+        //u_data_out[running_index] <= user_data_out;
+        //running_index++;
+
+        for(int b = 0; b < u_b_len[i] + 1; b++)
+        begin
+       
+            //running_index++;
+            
+            //wait(user_stall_data == 0);
+            wait(user_data_out_en == 1);
+            @(posedge aclk);
+            //@(posedge aclk);
+            
+            //@(posedge aclk_out);
+            u_data_out[running_index] = user_data_out;
+            running_index++;
+        end
+
+        user_start          = 1'd0;
     end
 
-// AXI READ ---------------------------------------------------    
-    always @ (*)
+////////////////////////////////////////////////////////////////////////    
+/*
+    wait(user_free);
+    #10us;
+   
+    running_index   = 'd0;
+    user_w_r = 'h1;
+    
+    for(int i = 0; i < 10; i++)
     begin
-        m_axi_araddr      <= ((axi_cs==IDLE) & (user_w_r)) ? user_addr_in : 0;
-        m_axi_arlen       <= ((axi_cs==IDLE) & (user_w_r)) ? user_burst_len_in : 0;
-        m_axi_arvalid     <= ((axi_cs==IDLE) & (user_w_r)) ? 1 : 0;
-        m_axi_rready      <= (axi_cs==READ_RESPONSE) ? 1 : 0;
-        user_data_out     <= #1 (axi_cs==READ_RESPONSE) ? m_axi_rdata : 0;
-        user_data_out_en  <= #1 (axi_cs==READ_RESPONSE) ? m_axi_rvalid : 0;
+        wait(user_free);
+        @(posedge aclk);
+        
+        user_addr_in        = u_addr[i];
+        user_burst_len_in   = u_b_len[i];
+        //user_pixels_1_2     = u_pix_len[i];
+        //user_data_in        = u_data_in[running_index];
+        user_start          = 1'd1;
+        
+        @(posedge aclk);
+        
+        for(int b = 0; b < u_b_len[i]+1; b++)
+        begin
+            //@(posedge user_data_out_en);
+            if(~user_data_out_en) wait(user_data_out_en);
+            @(posedge aclk);
+            if(user_stall_data) wait(~user_stall_data);
+            u_data_out[running_index] = user_data_out;
+            running_index++;
+        end
+        
+        user_start          = 1'd0;
     end
+*/
+//////////////////////////////////////////////////////////////////////////////////    
+    #10us;
+    running_index   = 'd0;
+    
+    current_addr = 0;
 
-// STATUS   ---------------------------------------------------
-    always @ (posedge aclk)
+    for(cmp_it = 0; cmp_it < 10; cmp_it++)
     begin
-
-        if(((axi_cs == WRITE_RESPONSE)&& m_axi_bvalid))
+    
+        current_addr = u_addr[cmp_it];
+    
+        for(int i = 0; i < u_b_len[cmp_it]+1; i++)
         begin
-            user_status <= m_axi_bresp;
-        end
-
-        else if(((axi_cs == READ_RESPONSE)&& m_axi_rvalid))
-        begin
-            user_status <= m_axi_rresp;
-        end
         
-        else
-        begin
-            user_status <= 'h0;
-        end
+            current_addr = current_addr + ((`DATA_W)*i);
+            cmp_data_diff = u_data_in[running_index] ^ u_data_out[running_index];
+        
+            if(|cmp_data_diff)
+            begin
+                $display("ADDRESS: %X, BURST LENGTH: %d, DATA WRITTEN: %X, DATA READ: %X, NOT EQUAL", current_addr, u_b_len[cmp_it]+1, u_data_in[running_index], u_data_out[running_index]);
+            end
+            
+            else
+            begin
+                $display("ADDRESS: %X, BURST LENGTH: %d, DATA WRITTEN: %X, DATA READ: %X, EQUAL", current_addr, u_b_len[cmp_it]+1, u_data_in[running_index], u_data_out[running_index]);
+            end
+            
+            running_index++;
+         end
     end
+    
+    $finish;
+end
+    
+design_1_wrapper d1w0(
+    .aclk_0(aclk),
+    .aresetn_0(aresetn),
+    .user_addr_in_0(user_addr_in),
+    .user_burst_len_in_0(user_burst_len_in),
+    .user_data_in_0(user_data_in),
+    .user_data_out_0(user_data_out),
+    .user_data_out_en_0(user_data_out_en),
+    .user_data_strb_0(user_pixels_1_2),
+    .user_free_0(user_free),
+    .user_stall_data_0(user_stall_data),
+    .user_start_0(user_start),
+    .user_status_0(user_status),
+    .user_w_r_0(user_w_r)
+    );
 
-    always @ (*)
-    begin
-        //user_stall_data = (~m_axi_wready) ? 1'b0 : 1'b1;
-        //user_stall_data = (((axi_cs == WRITE) & ~m_axi_wready) | ((axi_cs==READ_RESPONSE) & ~m_axi_rvalid)) ? 1'b0 : 1'b1;
-        #1;   
-        if((axi_cs == WRITE) & ~m_axi_wready)
-        begin
-            user_stall_data = 1'b1;
-        end
-        
-        else if((axi_cs == READ_RESPONSE) & ~m_axi_rvalid)
-        begin
-            user_stall_data = 1'b1;
-        end
-        
-        else user_stall_data = 1'b0;
-        
-        user_free       = (axi_ns == IDLE) ? 1'b1 : 1'b0;
-    end
-// STATUS   ---------------------------------------------------
 endmodule
